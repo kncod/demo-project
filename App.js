7e1d17ce35b977e8e957c7848b332d20817941ab
unknown <nigus2012bh@gmail.com>
2024-07-03 16:31:07 -0700

    with google Ahuthenticaton

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NativeWind } from 'nativewind';
+import {ClerkProvider} from '@clerk/clerk-expo';
 import LoginScreen from './Apps/Screen/LoginScreen';
+import { SignedIn } from '@clerk/clerk-expo';
+import { SignedOut } from '@clerk/clerk-expo';
 
 export default function App() {
   return (
+    <ClerkProvider publishableKey='pk_test_YWNjdXJhdGUtcG9zc3VtLTgwLmNsZXJrLmFjY291bnRzLmRldiQ'>
     <View className="flex-1  bg-white">
-      <StatusBar style="auto" />
+      <SignedIn>
+        <Text>you are signed in</Text>
+      </SignedIn>
+      <SignedOut>
       <LoginScreen/>
+      </SignedOut>
+      <StatusBar style="auto" />
+      
     </View>
+    </ClerkProvider>
   );
 }
 
--- Apps/Screen/LoginScreen.jsx
+++ Apps/Screen/LoginScreen.jsx
@@ -1,20 +1,82 @@
-import { View, Text,Image , TouchableOpacity} from 'react-native'
-import React from 'react'
+import React from "react";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Linking from "expo-linking";
+import { useOAuth } from "@clerk/clerk-expo";
 
-export default function LoginScreen() {
+export const useWarmUpBrowser = () => {
+  React.useEffect(() => {
+    // Warm up the browser to improve UX
+    WebBrowser.warmUpAsync();
+    return () => {
+      WebBrowser.coolDownAsync();
+    };
+  }, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
+
+const SignInWithOAuth = () => {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = React.useCallback(async () => {
+    try {
+      const { createdSessionId, setActive } = await startOAuthFlow({
+        redirectUrl: Linking.createURL("/dashboard", { scheme: "myapp" }),
+      });
+
+      if (createdSessionId && setActive) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Handle other flows like signIn or signUp
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }, [startOAuthFlow]);
+
   return (
     <View>
-        <Image source={require('./../../assets/images/home.jpg')}
-        className="w-full h-[400px] object-cover"
+      <View>
+        <Image
+          source={require("./../../assets/images/home.jpg")}
+          style={{ width: "100%", height: 400, resizeMode: "cover" }}
         />
-       <View className="p-8 bg-white mt-[-20px] rounded-t-3xl shadow-md">
-            <Text className="text-[30px]">RentEase Marketplace</Text>
-            <Text className="text-[18px] text-slate-500 mt-6">Buy Sell Merketplace where you can sell old item and make real money</Text>
-        <TouchableOpacity onPress={()=>console.log("SignIn")} className="p-4  bg-blue-500 rounded-full mt-20">
-            <Text className="text-white text-center text-[18px]">Get Started</Text>
-    </TouchableOpacity>
+        <View
+          style={{
+            padding: 16,
+            backgroundColor: "white",
+            marginTop: -20,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            shadowOpacity: 0.5,
+          }}
+        >
+          <Text style={{ fontSize: 30 }}>RentEase Marketplaces</Text>
+          <Text style={{ fontSize: 18, color: "slategray", marginTop: 6 }}>
+            Buy Sell Marketplace where you can sell old items and make real
+            money
+          </Text>
+          <TouchableOpacity
+            onPress={onPress}
+            style={{
+              padding: 16,
+              backgroundColor: "blue",
+              borderRadius: 50,
+              marginTop: 20,
+            }}
+          >
+            <Text style={{ color: "white", textAlign: "center", fontSize: 18 }}>
+              Get Started
+            </Text>
+          </TouchableOpacity>
         </View>
-
+      </View>
     </View>
-  )
-}
+  );
+};
+
+export default SignInWithOAuth;
+
